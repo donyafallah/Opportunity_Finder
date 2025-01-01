110e8aa4aca18aee09eed99996f80cff75aa6e9e
Donyafallah <donyafallah2004@gmail.com>
2025-01-01 15:26:29 +0330

    add chart

--- Result.js
+++ Result.js
@@ -1,22 +1,54 @@
 
 function showContent(tabName) {
-  // گرفتن همه دکمه ها
+
   const tabs = document.getElementsByClassName('tab-link');
 
-  // حذف کلاس active از همه دکمه ها
+
   for (let i = 0; i < tabs.length; i++) {
     tabs[i].classList.remove('active');
   }
 
-  // اضافه کردن کلاس active به دکمه فعلی
+
   event.target.classList.add('active');
 
-  // پنهان کردن همه بخش های محتوا
+
   const contentAreas = document.getElementsByClassName('tab-content');
   for (let i = 0; i < contentAreas.length; i++) {
     contentAreas[i].style.display = 'none';
   }
 
-  // نمایش بخش محتوای مورد نظر
+
   document.getElementById(tabName).style.display = 'block';
 }
+
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+
+var canvas = document.getElementById('myChart');
+canvas.width = 400; 
+canvas.height = 300; 
+
+
+var myChart = new Chart(ctx, {
+    type: 'bar', 
+    data: {
+        labels: ['ایرانسل', 'داتین', 'جنگل', 'مجتمع فنی تهران'],
+        datasets: [{
+            label: 'وضعیت رزومه‌ها',
+            data: [5, 3, 2, 6], 
+            backgroundColor: 'rgba(87, 36, 100, 0.7)', 
+            borderColor: 'rgba(87, 36, 100, 1)', 
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true 
+            }
+        }
+    }
+});
+
+
